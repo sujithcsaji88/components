c5d576535485b50569d943b4ea2c434c740890d3
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-05-28 13:52:08 -0400

    Used sample JSON file with 20,000 records

--- grid/src/App.js
+++ grid/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import offerData from "./getofferlist.json";
+import sampleData from "./sample_data.json";
 import BootstrapTable from "react-bootstrap-table-next";
 import paginationFactory from "react-bootstrap-table2-paginator";
 
@@ -11,26 +11,59 @@ const arrayFormatter = (cell, row) => {
 	return cell.join(",");
 };
 
+const objectFormatter = (cell, row) => {
+	const names = cell.map((friend) => {
+		return friend.name;
+	});
+	return names.join(",");
+};
+
+const imageFormatter = (cell, row) => {
+	return <img src={cell} style={{ width: "100%", height: "100%" }} alt='Employee DP' />;
+};
+
+const rowStyle = (row, rowIndex) => {
+	return { wordBreak: "break-all" };
+};
+
 const App = () => {
 	const columns = [
-		{ dataField: "id", text: "Offer Code", formatter: styleFormatter },
-		{ dataField: "type", text: "Offer Type" },
-		{ dataField: "status", text: "Offer Status" },
-		{ dataField: "displayid", text: "displayid" },
-		{ dataField: "title", text: "title" },
-		{ dataField: "description", text: "description" },
-		{ dataField: "start", text: "start date" },
-		{ dataField: "end", text: "end date" },
-		{ dataField: "source", text: "source" },
-		{ dataField: "pref", text: "pref" },
-		{ dataField: "ruleId", text: "ruleId" },
-		{ dataField: "type", text: "type" },
-		{ dataField: "propertyList", text: "propertyList", formatter: arrayFormatter },
+		{ dataField: "index", text: "Index", formatter: styleFormatter },
+		{ dataField: "id", text: "Data ID" },
+		{ dataField: "guid", text: "Global ID" },
+		{ dataField: "isActive", text: "Is Active" },
+		{ dataField: "balance", text: "Balance Amount" },
+		{ dataField: "picture", text: "Image", formatter: imageFormatter },
+		{ dataField: "age", text: "Age" },
+		{ dataField: "eyeColor", text: "Eye Color" },
+		{ dataField: "name", text: "Name" },
+		{ dataField: "gender", text: "Gender" },
+		{ dataField: "company", text: "Company" },
+		{ dataField: "email", text: "Email ID" },
+		{ dataField: "phone", text: "Phone Number" },
+		{ dataField: "address", text: "Address" },
+		{ dataField: "registered", text: "Registered Time" },
+		{ dataField: "latitude", text: "Latitude" },
+		{ dataField: "longitude", text: "Longitude" },
+		{ dataField: "tags", text: "Tags", formatter: arrayFormatter },
+		{ dataField: "friends", text: "Friends", formatter: objectFormatter },
+		{ dataField: "favoriteFruit", text: "Favorite Fruit" },
 	];
 
+	const pagination = paginationFactory({
+		sizePerPage: 50,
+	});
+
+	console.log("JSON Length: " + sampleData.length);
 	return (
 		<div className='App'>
-			<BootstrapTable keyField='id' data={offerData.offers} columns={columns}></BootstrapTable>
+			<BootstrapTable
+				keyField='id'
+				data={sampleData}
+				columns={columns}
+				rowStyle={rowStyle}
+				pagination={pagination}
+			></BootstrapTable>
 		</div>
 	);
 };
