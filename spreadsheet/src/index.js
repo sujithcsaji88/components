db4d5b7835495677826b6f70c5eb225042eba37c
Abhilash R Jayakumar <62945844+abhlashrj@users.noreply.github.com>
2020-06-01 14:24:00 -0400

    Code cleanup

--- spreadsheet/src/index.js
+++ spreadsheet/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import './examples.scss';
 
 ReactDOM.render(
@@ -11,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
