42102c9e995e1baa505169e0fafa270962d4605b
Abhilash R Jayakumar <62945844+abhlashrj@users.noreply.github.com>
2020-06-01 14:27:00 -0400

    Updated feed

--- spreadsheet/src/slickgrid.js
+++ spreadsheet/src/slickgrid.js
@@ -7,6 +7,18 @@ const columns = [
   {id: '%', name: '% Complete', field: 'percentComplete'},
   {id: 'start', name: 'Start', field: 'start'},
   {id: 'finish', name: 'Finish', field: 'finish'},
+  {id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven'},
+  {id: 'title', name: 'Title', field: 'title', maxWidth: 100, minWidth: 80},
+  {id: 'duration', name: 'Duration', field: 'duration', resizable: false},
+  {id: '%', name: '% Complete', field: 'percentComplete'},
+  {id: 'start', name: 'Start', field: 'start'},
+  {id: 'finish', name: 'Finish', field: 'finish'},
+  {id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven'},
+  {id: 'title', name: 'Title', field: 'title', maxWidth: 100, minWidth: 80},
+  {id: 'duration', name: 'Duration', field: 'duration', resizable: false},
+  {id: '%', name: '% Complete', field: 'percentComplete'},
+  {id: 'start', name: 'Start', field: 'start'},
+  {id: 'finish', name: 'Finish', field: 'finish'},
   {id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven'}
 ];
 const options = {
