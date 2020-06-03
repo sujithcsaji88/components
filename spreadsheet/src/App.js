6070e700821a6d03e53fc85752ca01fb798e24ef
Krishna Kumar <ggkrish3093@gmail.com>
2020-06-03 19:12:14 +0530

    Conflict fixed

--- spreadsheet/src/App.js
+++ spreadsheet/src/App.js
@@ -1,22 +1,16 @@
-import React , { Component,useEffect } from 'react';
-import spreadsheet from './components/slickgrid'	;
-import Header from './components/Header/Header';
+import React, { useEffect } from "react";
+import spreadsheet from "./components/slickgrid";
+import Header from "./components/Header/Header";
 
 const App = () => {
   useEffect(() => {
     spreadsheet();
-  }
-  render () {
-    return (
-      <div>
-        <Header/>
-          <div id="iCargoSpreadSheet"  className="slickgrid-container"></div>
-      </div>
-    );
-  }
-}
-export default App;
   }, []);
+  return (
+    <div>
+      <Header />
+      <div id="iCargoSpreadSheet" className="slickgrid-container"></div>
+    </div>
+  );
 };
-
 export default App;
