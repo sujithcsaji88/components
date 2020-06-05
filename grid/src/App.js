cbdf9c12a89a70382d25f4eeca961f51fa6f6e51
ssoman <ssoman@accelaero.com>
2020-06-05 18:45:59 +0530

    Apply Style and segment column filter

--- grid/src/App.js
+++ grid/src/App.js
@@ -22,7 +22,7 @@ const $ = window.$;
 const flightFormatter = (cell) => {
 	return (
 		<div>
-			<p style={{ color: "red" }}>{cell.flightno}</p>
+			<p className="flightCol">{cell.flightno}</p>
 			<p>{cell.date}</p>
 		</div>
 	);
@@ -32,7 +32,9 @@ const segmentFormatter = (cell) => {
 	return (
 		<div>
 			<p>
-				{cell.from} - {cell.to}
+				{cell.from} 
+					<img className="img" alt='' src="https://d1icd6shlvmxi6.cloudfront.net/gsc/OYDDTS/35/1e/62/351e623259f345efac57e1f95b7849f9/images/neo-list-5/u374.png?token=f5979504349fe3dc90a3076a078178c0306f3004b31e4e2956f3bdb677a59ec9" />
+				{cell.to}
 			</p>
 		</div>
 	);
@@ -40,12 +42,16 @@ const segmentFormatter = (cell) => {
 
 const detailsFormatter = (cell) => {
 	return (
-		<div>
+		<div >
 			<p>
 				{cell.startTime} - {cell.endTime} | {cell.status} | {cell.additionalStatus}
 			</p>
 			<p>
-				{cell.flightModel} | {cell.bodyType} | {cell.type} | {cell.timeStatus}
+				<div className='d-flex'>
+					{cell.flightModel} | {cell.bodyType} | {cell.type} | 
+					<div className="detailsCol">{cell.timeStatus.split('to')[0]}</div>
+					<div> {' to depart'}</div>
+				</div>
 			</p>
 		</div>
 	);
@@ -54,8 +60,12 @@ const detailsFormatter = (cell) => {
 const weightAndVolumeFormatter = (cell) => {
 	return (
 		<div>
-			<p>{cell.percentage}</p>
-			<p>{cell.value}</p>
+			<p className="weightCol">{cell.percentage}</p>
+			<div className='d-flex'>
+				<p className="weightValueCol">{cell.value.split('/')[0]}</p>
+				<p className="weightValueSecondCol">/{cell.value.split('/')[1].split(' ')[0]}</p>
+				<p>{' '+cell.value.split('/')[1].split(' ')[1]}</p>
+			</div>
 		</div>
 	);
 };
@@ -64,9 +74,11 @@ const positionFormatter = (cell) => {
 	return cell.map((positions, index) => {
 		return (
 			<div key={index} style={{ width: "50%", float: "left", textAlign: "center" }}>
-				<p>
-					{positions.position} {positions.value}
-				</p>
+				<div className='d-flex'>
+					<p> {positions.position} </p>
+					<p className='uldCol'> {positions.value} </p>
+				</div>
+				
 			</div>
 		);
 	});
@@ -74,9 +86,17 @@ const positionFormatter = (cell) => {
 
 const revenueFormatter = (cell) => {
 	return (
-		<div>
+		<div className='revenueCol'>
 			<p>{cell.revenue}</p>
 			<p>{cell.yeild}</p>
+		</div>
+	);
+};
+
+const srFormatter = (cell) => {
+	return (
+		<div className='srCol'>
+			<p>{cell}</p>
 		</div>
 	);
 };
@@ -84,8 +104,16 @@ const revenueFormatter = (cell) => {
 const bookingFormatter = (cell) => {
 	return (
 		<div>
-			<p>{cell.sr}</p>
-			<p>{cell.volume}</p>
+			<div className='row'>
+				<p className='bookingCol'>{cell.sr.split(' ')[0]}</p>
+				<p>{cell.sr.split(' ')[1]}</p>
+			</div>
+			<div className='row'>
+				<p className='bookingCol'>{cell.volume.split('/')[0].split(' ')[0]}</p>
+				<p>{cell.volume.split('/')[0].split(' ')[1]}</p>
+				<p className='bookingCol'>{' / '+cell.volume.split('/')[1].split(' ')[1]}</p>
+				<p>{cell.volume.split('/')[1].split(' ')[2]}</p>
+			</div>
 		</div>
 	);
 };
@@ -104,10 +132,23 @@ const expandRow = {
 
 const customFlightFilter = (filterVal, data) => {
 	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
 		return data.filter(
 			(travel) =>
-				travel.flight.date.toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.flight.flightno.toLowerCase().includes(filterVal.toLowerCase())
+				travel.flight.date.toLowerCase().includes(filterVal) ||
+				travel.flight.flightno.toLowerCase().includes(filterVal)
+		);
+	}
+	return data;
+};
+
+const customSegmentFilter = (filterVal, data) => {
+	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
+		return data.filter(
+			(travel) =>
+				travel.segment.from.toLowerCase().includes(filterVal) ||
+				travel.segment.to.toLowerCase().includes(filterVal)
 		);
 	}
 	return data;
@@ -115,16 +156,17 @@ const customFlightFilter = (filterVal, data) => {
 
 const customDetailsFilter = (filterVal, data) => {
 	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
 		return data.filter(
 			(travel) =>
-				travel.details.flightModel.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.bodyType.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.type.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.startTime.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.endTime.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.status.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.additionalStatus.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.details.timeStatus.toString().toLowerCase().includes(filterVal.toLowerCase())
+				travel.details.flightModel.toString().toLowerCase().includes(filterVal) ||
+				travel.details.bodyType.toLowerCase().includes(filterVal) ||
+				travel.details.type.toLowerCase().includes(filterVal) ||
+				travel.details.startTime.toLowerCase().includes(filterVal) ||
+				travel.details.endTime.toLowerCase().includes(filterVal) ||
+				travel.details.status.toLowerCase().includes(filterVal) ||
+				travel.details.additionalStatus.toLowerCase().includes(filterVal) ||
+				travel.details.timeStatus.toLowerCase().includes(filterVal)
 		);
 	}
 	return data;
@@ -132,10 +174,11 @@ const customDetailsFilter = (filterVal, data) => {
 
 const customWeightFilter = (filterVal, data) => {
 	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
 		return data.filter(
 			(travel) =>
-				travel.weight.percentage.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.weight.value.toString().toLowerCase().includes(filterVal.toLowerCase())
+				travel.weight.percentage.toLowerCase().includes(filterVal) ||
+				travel.weight.value.toLowerCase().includes(filterVal)
 		);
 	}
 	return data;
@@ -143,10 +186,11 @@ const customWeightFilter = (filterVal, data) => {
 
 const customVolumeFilter = (filterVal, data) => {
 	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
 		return data.filter(
 			(travel) =>
-				travel.volume.percentage.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.volume.value.toString().toLowerCase().includes(filterVal.toLowerCase())
+				travel.volume.percentage.toLowerCase().includes(filterVal) ||
+				travel.volume.value.toLowerCase().includes(filterVal)
 		);
 	}
 	return data;
@@ -154,10 +198,11 @@ const customVolumeFilter = (filterVal, data) => {
 
 const customRevenueFilter = (filterVal, data) => {
 	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
 		return data.filter(
 			(travel) =>
-				travel.revenue.revenue.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.revenue.yeild.toString().toLowerCase().includes(filterVal.toLowerCase())
+				travel.revenue.revenue.toLowerCase().includes(filterVal) ||
+				travel.revenue.yeild.toLowerCase().includes(filterVal)
 		);
 	}
 	return data;
@@ -165,10 +210,11 @@ const customRevenueFilter = (filterVal, data) => {
 
 const customQueuedBookingFilter = (filterVal, data) => {
 	if (filterVal) {
+		filterVal = filterVal.toLowerCase();
 		return data.filter(
 			(travel) =>
-				travel.queuedBooking.sr.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-				travel.queuedBooking.volume.toString().toLowerCase().includes(filterVal.toLowerCase())
+				travel.queuedBooking.sr.toLowerCase().includes(filterVal) ||
+				travel.queuedBooking.volume.toLowerCase().includes(filterVal)
 		);
 	}
 	return data;
@@ -176,42 +222,43 @@ const customQueuedBookingFilter = (filterVal, data) => {
 
 const customUldFilter = (filterVal, data) => {
 	if (filterVal) {
-		//var searchVal = values.position.toString().toLowerCase() + ' '+ values.value.toString().toLowerCase();
+		filterVal = filterVal.toLowerCase();
 		return data.filter((travel) =>{
-				const {uldPositions} = travel;
-				const filteredData = uldPositions.filter(values => 
-					values.position.toString().toLowerCase().includes(filterVal.toLowerCase()) ||
-					values.value.toString().toLowerCase().includes(filterVal.toLowerCase())
-				);
-				return filteredData.length>0
-
-	})
-};
-return data;
+			const {uldPositions} = travel;
+			const filteredData = uldPositions.find(values => {
+				var searchVal = values.position + ' '+ values.value;
+				if(searchVal.toLowerCase().includes(filterVal))
+					return true
+			}	
+		);
+		return filteredData
+		})
+	};
+	return data;
 }
 
 function onColumnMatch({ searchText, value, column, row }) {
 	if (searchText) {
+		searchText = searchText.toLowerCase();
 		return value.filter((travel) =>
-			travel.flight.date.toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.flight.flightno.toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.flightModel.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.bodyType.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.type.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.startTime.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.endTime.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.status.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.additionalStatus.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.details.timeStatus.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.weight.percentage.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.weight.value.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.volume.percentage.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.volume.value.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.revenue.revenue.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.revenue.yeild.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.sr.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.queuedBooking.sr.toString().toLowerCase().includes(searchText.toLowerCase()) ||
-			travel.queuedBooking.volume.toString().toLowerCase().includes(searchText.toLowerCase())
+			travel.flight.date.toLowerCase().includes(searchText) ||
+			travel.flight.flightno.toLowerCase().includes(searchText) ||
+			travel.details.flightModel.toString().toLowerCase().includes(searchText) ||
+			travel.details.bodyType.toLowerCase().includes(searchText) ||
+			travel.details.type.toLowerCase().includes(searchText) ||
+			travel.details.startTime.toLowerCase().includes(searchText) ||
+			travel.details.endTime.toLowerCase().includes(searchText) ||
+			travel.details.status.toLowerCase().includes(searchText) ||
+			travel.details.additionalStatus.toLowerCase().includes(searchText) ||
+			travel.details.timeStatus.toLowerCase().includes(searchText) ||
+			travel.weight.percentage.toLowerCase().includes(searchText) ||
+			travel.weight.value.toLowerCase().includes(searchText) ||
+			travel.volume.percentage.toLowerCase().includes(searchText) ||
+			travel.volume.value.toLowerCase().includes(searchText) ||
+			travel.revenue.revenue.toLowerCase().includes(searchText) ||
+			travel.revenue.yeild.toLowerCase().includes(searchText) ||
+			travel.queuedBooking.sr.toLowerCase().includes(searchText) ||
+			travel.queuedBooking.volume.toLowerCase().includes(searchText)
 		);
 	}
 	return value;
@@ -235,7 +282,9 @@ const App = () => {
 			dataField: "segment",
 			text: "Segment",
 			formatter: segmentFormatter,
-			filter: textFilter(),
+			filter: textFilter({
+				onFilter: customSegmentFilter,
+			}),
 			editorRenderer: (editorProps, value, row, column, rowIndex, columnIndex) => (
 				<SegmentEdit {...editorProps} value={value} airports={airports} />
 			),
@@ -247,7 +296,7 @@ const App = () => {
 			filter: textFilter({
 				onFilter: customDetailsFilter,
 			}),
-			headerStyle: { width: "18%" },
+			headerStyle: { width: "20%" },
 			editorRenderer: (editorProps, value, row, column, rowIndex, columnIndex) => (
 				<DetailsEdit {...editorProps} value={value} />
 			),
@@ -296,7 +345,7 @@ const App = () => {
 				<RevenueEdit {...editorProps} value={value} />
 			),
 		},
-		{ dataField: "sr", text: "SR", filter: textFilter() },
+		{ dataField: "sr", text: "SR", formatter: srFormatter, filter: textFilter() },
 		{
 			dataField: "queuedBooking",
 			text: "Queued Booking",
@@ -313,8 +362,6 @@ const App = () => {
 		sizePerPage: 50,
 	});
 
-	console.log("JSON Length: " + sampleData.length);
-
 	useEffect(() => {
 		$(".react-bootstrap-table thead th").resizable();
 	});
@@ -325,9 +372,27 @@ const App = () => {
 			<ToolkitProvider keyField='travelId' data={sampleData} columns={columns} search={ { searchFormatted: true } }>
 				{(props) => (
 					<div>
-						<div className='row m-2 col-md-12 searchArea'>
-							<div className='col-md-6'>
-								<SearchBar {...props.searchProps} className='searchInput' />
+						<div className='row m-0 p-3 col-md-12' style={{background: '#1f567e0c'}}>
+							<div className='col-md-6'></div>
+							<div className='col-md-6 row searchArea'>
+								<div className='col-md-4 searchAreaElement'>
+									<SearchBar {...props.searchProps} />
+								</div>
+								<div className='col-md-1'>
+									<i className="fa fa-sort-amount-desc fa-2x fa-border"></i>
+								</div>
+								<div className='col-md-1'>
+									<i className="fa fa-columns fa-2x fa-border" aria-hidden="true"></i>
+								</div>
+								<div className='col-md-1'>
+									<i className="fa fa-refresh fa-2x fa-border" aria-hidden="true"></i>
+								</div>
+								<div className='col-md-1'>
+									<i className="fa fa-cog fa-2x fa-border" aria-hidden="true"></i>
+								</div>
+								<div className='col-md-1'>
+									<span className="fa fa-file-excel-o fa-2x fa-border" />
+								</div>
 							</div>
 						</div>
 						<BootstrapTable
@@ -338,6 +403,7 @@ const App = () => {
 							cellEdit={cellEditFactory({ mode: "dbclick", blurToSave: true })}
 							pagination={pagination}
 							hover={true}
+							selectRow={ { mode: 'checkbox' } }
 						/>
 					</div>
 				)}
