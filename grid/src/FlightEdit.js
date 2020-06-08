a251e6aa55adf2fafde8e720691f3b1e6bc46f9b
skhan8405 <shahrukh.khan.contractor@hilton.com>
2020-06-08 11:59:04 +0530

    Cell Edit For Flight Column: Calendar Drop Down

--- grid/src/FlightEdit.js
+++ grid/src/FlightEdit.js
@@ -5,10 +5,24 @@ class FlightEdit extends Component {
 		if (this) {
 			const { flightno, date } = this;
 			if (flightno && date) {
-				return { flightno: flightno.value, date: date.value };
+				return { flightno: flightno.value, date: this.getDate(new Date(date.value)) };
 			}
 		}
 	}
+
+	getDate=(date, type)=>{
+		if(type ==="calendar"){
+			const dateTimeFormat = new Intl.DateTimeFormat("en-US", { year: "numeric", month: "2-digit", day: "2-digit" });
+			const [{ value: month }, , { value: day }, , { value: year }] = dateTimeFormat.formatToParts(date);
+			return`${year}-${month}-${day}`
+		}
+		else{
+			const dateTimeFormat = new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "2-digit" });
+			const [{ value: month }, , { value: day }, , { value: year }] = dateTimeFormat.formatToParts(date);
+			return`${year}-${month.toUpperCase()}-${day}`;
+		}
+	}
+	
 	render() {
 		const { value, onUpdate, ...rest } = this.props;
 		return [
@@ -23,9 +37,9 @@ class FlightEdit extends Component {
 			<input
 				{...rest}
 				key={value.date}
-				defaultValue={value.date}
+				defaultValue={this.getDate(new Date(value.date), "calendar")}
 				ref={(node) => (this.date = node)}
-				type='text'
+				type='date'
 				style={{ width: "100%", marginBottom: "10px" }}
 			/>,
 		];
@@ -33,3 +47,4 @@ class FlightEdit extends Component {
 }
 
 export default FlightEdit;
+
