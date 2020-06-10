87a86febed375728d0902251f5448586697828c3
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-06-10 14:50:09 -0400

    Outside click event handled for new column added

--- table/src/components/Cells/RowOptions.js
+++ table/src/components/Cells/RowOptions.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
 import RowDelete from "../../images/RowDelete.svg";
 import RowEdit from "../../images/RowEdit.svg";
 import RowPin from "../../images/RowPin.png";
@@ -15,44 +16,46 @@ const RowOptions = () => {
     };
 
     return (
-        <div className="row-options-edit-wrap">
-            <span className="icon-edit" onClick={openOverlay}>
-                <i></i>
-                <i></i>
-                <i></i>
-            </span>
-            <div className={`row-options-edit ${isOpen ? "open" : "close"}`}>
-                <ul>
-                    <li>
-                        <span>
-                            <i>
-                                <img src={RowEdit} alt="cargo" />
-                            </i>
-                            <span>Edit</span>
-                        </span>
-                    </li>
-                    <li>
-                        <span>
-                            <i>
-                                <img src={RowPin} alt="cargo" width="15" height="15" />
-                            </i>
-                            <span>Pin This row</span>
-                        </span>
-                    </li>
-                    <li>
-                        <span>
-                            <i>
-                                <img src={RowDelete} alt="cargo" />
-                            </i>
-                            <span>Delete</span>
-                        </span>
-                    </li>
-                </ul>
-                <span className="close" onClick={closeOverlay}>
-                    <i class="fa fa-close"></i>
+        <ClickAwayListener onClickAway={closeOverlay}>
+            <div className="row-options-edit-wrap">
+                <span className="icon-edit" onClick={openOverlay}>
+                    <i></i>
+                    <i></i>
+                    <i></i>
                 </span>
+                <div className={`row-options-edit ${isOpen ? "open" : "close"}`}>
+                    <ul>
+                        <li>
+                            <span>
+                                <i>
+                                    <img src={RowEdit} alt="cargo" />
+                                </i>
+                                <span>Edit</span>
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                <i>
+                                    <img src={RowPin} alt="cargo" width="15" height="15" />
+                                </i>
+                                <span>Pin This row</span>
+                            </span>
+                        </li>
+                        <li>
+                            <span>
+                                <i>
+                                    <img src={RowDelete} alt="cargo" />
+                                </i>
+                                <span>Delete</span>
+                            </span>
+                        </li>
+                    </ul>
+                    <span className="close" onClick={closeOverlay}>
+                        <i class="fa fa-close"></i>
+                    </span>
+                </div>
             </div>
-        </div>
+        </ClickAwayListener>
     );
 };
 
