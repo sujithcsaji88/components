12ad80252986c46d63762767b5c78110ffde57f4
Krishna Kumar <ggkrish3093@gmail.com>
2020-06-11 16:06:25 +0530

    changes for global search

--- spreadsheet/src/components/common/ErrorMessage.js
+++ spreadsheet/src/components/common/ErrorMessage.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { SEARCH_NOT_FOUNT_ERROR } from '../constants/ErrorConstants'
+import { SEARCH_NOT_FOUNT_ERROR } from "../constants/ErrorConstants";
 
 const ErrorMessage = (props) => {
-  if (props.status === "invalid") {
+
+  const [status, setStatus] = useState(props.status);
+
+  useEffect(() => {
+    setStatus(props.status);
+  }, [props.status]);
+
+  function hideErrorMessage() {
+    setStatus("");
+  }
+
+  if (status === "invalid") {
     return (
       <div id="errorMsg">
         <div className="alert alert-danger" role="alert">
           {SEARCH_NOT_FOUNT_ERROR}
         </div>
-        <div className="notification-close" onClick={hideErrorMessage}>
-          <FontAwesomeIcon icon={faTimes}/>
+        <div className="notification-close">
+          <FontAwesomeIcon icon={faTimes} onClick={hideErrorMessage} />
         </div>
       </div>
     );
   } else return <div></div>;
 };
-
-function hideErrorMessage() {
-    
-}
 
 export default ErrorMessage;
