f3935a41e490764145eef12a66ca2e44ec58fa94
Abhilash R Jayakumar <62945844+abhlashrj@users.noreply.github.com>
2020-06-11 16:31:33 -0400

    Filter component base framework & stubs to bind Filter data.

---
+++ filter/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import FilterData from './stubs/FilterData.json'
+
+function App() {
+  return (
+    <div className="App">
+         Filter -TODO
+    </div>
+  );
+}
+
+export default App;
