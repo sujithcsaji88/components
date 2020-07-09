27d4d3c1c7d732d195dcae06e8b283f0c12d59b3
jayakrishnanrnair <63115526+jayakrishnanrnair@users.noreply.github.com>
2020-07-09 18:48:49 -0400

    Opacity and text selection of column box that is being dragged

--- spreadsheet/src/components/overlays/column_chooser/columnItem.js
+++ spreadsheet/src/components/overlays/column_chooser/columnItem.js
@@ -3,44 +3,44 @@ import { useDrag, useDrop } from "react-dnd";
 import { ItemTypes } from "./ItemTypes";
 
 const style = {
-    cursor: "move"
+	cursor: "move",
 };
 
 const ColumnItem = ({ id, text, moveColumn, findColumn }) => {
-    const originalIndex = findColumn(id).index;
+	const originalIndex = findColumn(id).index;
 
-    const [{ isDragging }, drag] = useDrag({
-        item: { type: ItemTypes.COLUMN, id, originalIndex },
-        collect: (monitor) => ({
-            isDragging: monitor.isDragging()
-        }),
-        end: (dropResult, monitor) => {
-            const { id: droppedId, originalIndex } = monitor.getItem();
-            const didDrop = monitor.didDrop();
-            if (!didDrop) {
-                moveColumn(droppedId, originalIndex);
-            }
-        }
-    });
+	const [{ isDragging }, drag] = useDrag({
+		item: { type: ItemTypes.COLUMN, id, originalIndex },
+		collect: (monitor) => ({
+			isDragging: monitor.isDragging(),
+		}),
+		end: (dropResult, monitor) => {
+			const { id: droppedId, originalIndex } = monitor.getItem();
+			const didDrop = monitor.didDrop();
+			if (!didDrop) {
+				moveColumn(droppedId, originalIndex);
+			}
+		},
+	});
 
-    const [, drop] = useDrop({
-        accept: ItemTypes.COLUMN,
-        canDrop: () => false,
-        hover({ id: draggedId }) {
-            if (draggedId !== id) {
-                const { index: overIndex } = findColumn(id);
-                moveColumn(draggedId, overIndex);
-            }
-        }
-    });
+	const [, drop] = useDrop({
+		accept: ItemTypes.COLUMN,
+		canDrop: () => false,
+		hover({ id: draggedId }) {
+			if (draggedId !== id) {
+				const { index: overIndex } = findColumn(id);
+				moveColumn(draggedId, overIndex);
+			}
+		},
+	});
 
-    const opacity = isDragging ? 0.5 : 1;
+	const opacity = isDragging ? 0.1 : 1;
 
-    return (
-        <div ref={(node) => drag(drop(node))} style={{ ...style, opacity }}>
-            {text}
-        </div>
-    );
+	return (
+		<div ref={(node) => drag(drop(node))} style={{ ...style, opacity }}>
+			{text}
+		</div>
+	);
 };
 
 export default ColumnItem;
