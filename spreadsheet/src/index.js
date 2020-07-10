f545c04ebddc2d4fe3e380ac08a26f874fd23ccb
djnadackal <djnadackal@gmail.com>
2020-07-10 22:57:45 +0530

    removed a unnecessary file
    removed a unnecessary file

--- spreadsheet/src/index.js
+++ spreadsheet/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import Try from "../../spreadsheet/src/components/overlays/sorting/try"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
